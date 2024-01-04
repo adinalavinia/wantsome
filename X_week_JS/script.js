39af0eb66ab1a1f37f8e64eff24df1be975b6525
Adina <adinaa_lavinia@yahoo.com>
2024-01-04 20:45:20 +0200

    js

--- X_week_JS/script.js
+++ X_week_JS/script.js
@@ -10,3 +10,30 @@
 }
 
   findLongestWord("The quick brown fox jumped over the lazy dog");
+
+
+
+
+
+  // Check if a string (first argument, str) ends with the given target string (second argument, target).
+
+
+  function confirmEnding(str, target) {
+    let currentWord = '';
+    let contorTarget = target.length - 1;
+  
+    for(let i = str.length - 1; i >= 0; i--){
+      
+      if(str[i] !== target[contorTarget]){
+        return false;
+      } 
+      contorTarget--;
+      if(contorTarget < 0){
+        break;
+      }
+    }
+  
+    return true;
+  }
+  
+  confirmEnding("Bastian", "n");
